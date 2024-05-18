a60034609f15d5e192ea56b6e9aefae410258ad0
Ramon Hoyo <ramonandreshoyodiaz@gmail.com>
2024-05-17 20:32:23 -0400

    solution

--- src/middleware.js
+++ src/middleware.js
@@ -6,6 +6,38 @@ class RateLimiterMiddleware {
 
   middleware() {
     return (req, res, next) => {
+      const { ip } = req;
+      const { maxRequests, timeWindow } = this.options;
+      let clientData = this.rateLimitStore.get(ip);
+      if (!clientData) {
+        clientData = {
+          firstRequest: Date.now(),
+          requests: 1,
+        };
+        this.rateLimitStore.set(ip, clientData);
+      }
+
+      if (clientData.requests > maxRequests && Date.now() - clientData.firstRequest < timeWindow) {
+        res.setHeader('X-RateLimit-Remaining', 0);
+        res.status(429).send('You have exceeded the rate limit. Please try again later.');
+        return;
+      }
+
+      clientData.requests += 1;
+      const elapsedTime = Date.now() - clientData.firstRequest;
+      if (elapsedTime > timeWindow) {
+        clientData.firstRequest = Date.now();
+        clientData.requests = 1;
+      }
+      this.rateLimitStore.set(ip, clientData);
+
+
+      res.setHeader('X-RateLimit-Remaining', maxRequests - clientData.requests);
+
+      res.setHeader('X-RateLimit-Limit', maxRequests);
+      res.setHeader('X-RateLimit-Remaining', maxRequests - clientData.requests);
+      res.setHeader('X-RateLimit-Reset', clientData.firstRequest + timeWindow - Date.now());
+
       next();
     };
   }
--- src/options.js
+++ src/options.js
@@ -1,3 +1,6 @@
-const rateLimiterOptions = {};
+const rateLimiterOptions = {
+  maxRequests: 5,
+  timeWindow: 1000,
+};
 
 module.exports = rateLimiterOptions;
